e5838afe7eb530f34b0c27617d345f04a49ad75d
storyn26383 <storyn26383@gmail.com>
2016-04-10 03:15:12 +0800

    submit btn icon

--- resources/views/eat/create.blade.php
+++ resources/views/eat/create.blade.php
@@ -124,7 +124,7 @@
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
-                                        <i class="fa fa-btn fa-sign-in"></i>送出
+                                        <i class="fa fa-btn fa-check"></i>送出
                                     </button>
                                 </div>
                             </div>
