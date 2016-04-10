526a5aa87d22fa8d0dc65bc7c8a17873614fc42e
storyn26383 <storyn26383@gmail.com>
2016-04-10 20:08:30 +0800

    修正日期時間格式的問題

--- app/Eat.php
+++ app/Eat.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -21,5 +22,10 @@ class Eat extends Model
         static::addGlobalScope('latest', function (Builder $builder) {
             $builder->latest('datetime');
         });
+    }
+
+    public function getDatetimeAttribute($value)
+    {
+        return (new Carbon($value))->format('Y-m-d H:i');
     }
 }
